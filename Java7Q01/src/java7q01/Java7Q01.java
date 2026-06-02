ed897de50f5933f34be9133b0a4d9c5f8c92e4a1
Arthur Nunes <nunes.arthur@academico.ifpb.edu.br>
2026-06-02 16:27:59 -0300

    Commit test

--- Java7Q01/src/java7q01/Java7Q01.java
+++ Java7Q01/src/java7q01/Java7Q01.java
@@ -2,7 +2,7 @@
 package java7q01;
 public class Java7Q01 {
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello World");
     }
     
 }
