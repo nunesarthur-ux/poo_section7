cb910e425da9de2c6e75718cfbf4d42e424c7e73
Arthur Nunes Tavares <nunes.arthur@academico.ifpb.edu.br>
2026-06-10 11:41:14 -0300

    feat: implementação das funções e separação do main

--- Inventário/src/inventário/Inventário.java
+++ Inventário/src/inventário/Inventário.java
@@ -4,61 +4,21 @@ import java.util.InputMismatchException;
 public class Inventário {
     public static void main(String[] args) {
         Scanner in = new Scanner (System.in);
-        int tempoNum;
-        int tempEstoque;
-        String tempNome;
-        double tempPreco;
-        String tempAtivo;
-        int tempQtd = -1;
-        int MaxSize = -1;
+        int tempQtd;
         int tempMenu;
-        while(tempQtd < 0){
-            try {
-                System.out.println("Insira o numero de produtos que deseja adicionar "
-                + "Ou insira zero(0) caso nao queira acrescentar produto\"); : ");
-                tempQtd = in.nextInt();
-                if(tempQtd < 0){
-                    System.out.println("Escreva um num positivo");
-                } 
-            }
-            catch(InputMismatchException e){
-                System.out.println("Você inseriu outro tipo de dado");
-                in.nextLine();
-            }
-        }
-        Produto[] produtos = new Produto[tempQtd];
+        tempQtd = getNumProducts(in);
         if (tempQtd == 0){
             System.out.println("Nao ha produtos");
         }
         
-        
         else{
+            Produto[] produtos = new Produto[tempQtd];
             addToInventory(produtos,in);
+            do{
+                tempMenu = getMenuOption(in);
+                executeMenuChoice(tempMenu,produtos,in);
+            }while(tempMenu != 0);
         }
-        
-        do{
-            tempMenu = getMenuOption(in);
-            switch(tempMenu){
-                case 1:
-                    displayInventory(produtos);
-                    break;
-                case 2:
-                    produtos[1].addToInventory(3);
-                    break;
-                case 3:
-                    produtos[1].deductFromInventory(3);
-                    break;
-                case 4:
-                    produtos[1].setAtivo("desativado");
-                    break;
-                case 0:
-                    System.out.println("Saindo...");
-                    break;
-            }
-        }while(tempMenu != 0);
-        
-         System.out.println("\n======= PRODUTOS CADASTRADOS =======");
-         displayInventory(produtos);
         in.close();    
     }
     
@@ -92,9 +52,29 @@ public class Inventário {
                 p[i] = produto;
         }
     }
-    public int getNumProducts(Produto[]p){
-        return p.length;
+    
+    public static int getNumProducts(Scanner in){
+        int tempQtd = -1;
+        while(tempQtd < 0){
+        try{
+
+            System.out.println("Insira o numero de produtos:");
+            tempQtd = in.nextInt();
+            if(tempQtd < 0){
+                System.out.println("Digite um valor positivo");
+                }
+            }
+        catch(InputMismatchException e){
+            System.out.println(
+                "Digite apenas numeros.");
+            in.nextLine();
+            }
+        }
+        return tempQtd;
     }
+    
+    
+    
     public static int getMenuOption(Scanner in){
         int tempMenu = -1;
         while(tempMenu < 0 || tempMenu > 4){
@@ -104,6 +84,8 @@ public class Inventário {
                 System.out.println("3.Deduzir Estoque");
                 System.out.println("4.Descontinuar Produto");
                 System.out.println("0.Sair");
+                System.out.println("Insira um numero: ");
+                tempMenu =in.nextInt();
                 if (tempMenu < 0 || tempMenu > 4){
                     System.out.println("INVALIDO!");
                 }
@@ -116,6 +98,98 @@ public class Inventário {
             }
         }
         return tempMenu;
+    }
+    
+    public static int getProductNumber(Produto[]p, Scanner in){
+        int productChoice = -1;
+        for(int i = 0; i < p.length-1; i++){
+            System.out.println("Opcao " + i + " - " + p[i].getNomeProd());
+        }
+        while(productChoice < 0 || productChoice >= p.length){
+            try{
+                productChoice = in.nextInt();
+            }
+            catch(InputMismatchException e){
+                System.out.println("Digite um indice valido ");
+                in.nextLine();
+            }
+        }
+        return productChoice;
+    }
+    
+    public static void addInventory(Produto[]p, Scanner in){
+        int productChoice;
+        int updateValue = -1;
+        productChoice = getProductNumber(p,in);
+        while(updateValue < 0){
+            try{
+                System.out.println("Qauntos produtos deseja adicionar? ");
+                updateValue = in.nextInt();
+                if(updateValue < 0){
+                    System.out.println("Digite um valor maior ou igual a zero.");
+                }
+                
+            }
+            catch(InputMismatchException e){
+                System.out.println("Digite uma quantia valida");
+                in.nextLine();
+            }
+        }
+        p[productChoice].addToInventory(updateValue);
+        
+    }
+    
+    public static void deductInventory(Produto[]p, Scanner in){
+        int productChoice;
+        int updateValue = -1;
+        productChoice = getProductNumber(p,in);
+        while(updateValue < 0){
+            try{
+                System.out.println("Quantos produtos deseja deduzir? ");
+                updateValue = in.nextInt();
+                if(updateValue < 0){
+                    System.out.println("Digite um valor maior ou igual a zero.");
+                }
+                
+            }
+            catch(InputMismatchException e){
+                System.out.println("Digite uma quantia valida");
+                in.nextLine();
+            }
+        }
+        p[productChoice].deductFromInventory(updateValue);
+        
+    }
+    
+    public static void discontinuelInventory(Produto[]p, Scanner in){
+        int productChoice = getProductNumber(p,in);
+        p[productChoice].setAtivo("desativado");
+        
+    }
+    
+    public static void executeMenuChoice( int getMenuOption,Produto[]p, Scanner in){
+        switch(getMenuOption){
+                case 1:
+                    System.out.println("\n == Exibir inventario == ");
+                    displayInventory(p);
+                    break;
+                case 2:
+                    System.out.println("\n== Adicionar Estoque ==");
+                    addInventory(p,in);
+                    break;
+                case 3:
+                    System.out.println("\n == Deduzir Estoque == ");
+                    deductInventory(p,in);
+                    break;
+                case 4:
+                    System.out.println("\n == Descontinuar produto ==");
+                    discontinuelInventory(p,in);
+                    break;
+                case 0:
+                    System.out.println("Saindo...");
+                    break;
+            }
+        
     }
 }
     
