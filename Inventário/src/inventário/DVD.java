7fb7c7b6e08bcaa26518293a2f64443b513a9cc3
Arthur Nunes Tavares <nunes.arthur@academico.ifpb.edu.br>
2026-06-11 16:33:59 -0300

    feat: implementação da herança

---
+++ Inventário/src/inventário/DVD.java
@@ -0,0 +1,54 @@
+package inventário;
+public class DVD extends Produto {
+    private int duracaoFilme;
+    private int classificacaoEtaria;
+    private String estudioCinematografico;
+
+    public DVD(int duracaoFilme, int classificacaoEtaria, String estudioCinematografico, 
+            int numeroItem, int qtdEstoque, String nomeProd, double precoItem, String ativo) {
+        super(numeroItem, qtdEstoque, nomeProd, precoItem, ativo);
+        this.duracaoFilme = duracaoFilme;
+        this.classificacaoEtaria = classificacaoEtaria;
+        this.estudioCinematografico = estudioCinematografico;
+    }
+    
+    @Override
+    public double getValorEstoque(){
+        return (double) super.getQtdEstoque() * super.getPrecoItem();
+    
+    }
+
+    public int getDuracaoFilme() {
+        return duracaoFilme;
+    }
+
+    public void setDuracaoFilme(int duracaoFilme) {
+        this.duracaoFilme = duracaoFilme;
+    }
+
+    public int getClassificacaoEtaria() {
+        return classificacaoEtaria;
+    }
+
+    public void setClassificacaoEtaria(int classificacaoEtaria) {
+        this.classificacaoEtaria = classificacaoEtaria;
+    }
+
+    public String getEstudioCinematografico() {
+        return estudioCinematografico;
+    }
+
+    public void setEstudioCinematografico(String estudioCinematografico) {
+        this.estudioCinematografico = estudioCinematografico;
+    }
+
+    @Override
+    public String toString() {
+        return "DVD" + "duracaoFilme = " + duracaoFilme + ", classificacaoEtaria = " + classificacaoEtaria
+                + ", estudioCinematografico = " + estudioCinematografico;
+    }
+    
+    
+    
+    
+}
