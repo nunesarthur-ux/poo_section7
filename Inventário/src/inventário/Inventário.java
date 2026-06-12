253a1ed81e8ec568b97f2321987f0cdfabec7616
Arthur Nunes Tavares <nunes.arthur@academico.ifpb.edu.br>
2026-06-11 23:16:53 -0300

    feat: implementação polimorfismo

--- Inventário/src/inventário/Inventário.java
+++ Inventário/src/inventário/Inventário.java
@@ -13,10 +13,14 @@ public class Inventário {
         
         else{
             Produto[] produtos = new Produto[tempQtd];
-            addToInventory(produtos,in);
+            for(int i = 0; i < produtos.length; i++){
+                addToInventory(produtos,in,i);
+            }
             do{
                 tempMenu = getMenuOption(in);
-                executeMenuChoice(tempMenu,produtos,in);
+                for(int i = 0; i < produtos.length; i++){
+                    executeMenuChoice(tempMenu,produtos,in,i);
+                }
             }while(tempMenu != 0);
         }
         in.close();    
@@ -29,13 +33,12 @@ public class Inventário {
                 System.out.println(produt);
             } 
     }
-    public static void addToInventory(Produto[]p, Scanner in){
+    public static void addToInventory(Produto[]p, Scanner in,int i){
         int tempoNum;
         int tempEstoque;
         String tempNome;
         double tempPreco;
         String tempAtivo;
-        for (int i = 0; i < p.length; i++){
                 System.out.println("Insira o numero do item: ");
                 tempoNum = in.nextInt();
                 System.out.println("Insira a quantidade em estoque: ");
@@ -50,7 +53,66 @@ public class Inventário {
                 tempAtivo = in.nextLine();
                 Produto produto = new Produto(tempoNum, tempEstoque, tempNome, tempPreco, tempAtivo);
                 p[i] = produto;
-        }
+    }
+    
+    public static void addCDToInventory(Produto[]p, Scanner in, int i){
+        int tempoNum;
+        int tempEstoque;
+        String tempNome;
+        double tempPreco;
+        String tempAtivo;
+        String tempArtista;
+        int tempNumDeMusicas;
+        String tempSelo;
+                System.out.println("Insira o numero do item: ");
+                tempoNum = in.nextInt();
+                System.out.println("Insira a quantidade em estoque: ");
+                tempEstoque = in.nextInt();
+                in.nextLine();
+                System.out.println("Insira o nome do produto: ");
+                tempNome = in.nextLine();
+                System.out.println("Valor do produto: ");
+                tempPreco = in.nextDouble();
+                in.nextLine();
+                System.out.println("Status do produto: ");
+                tempAtivo = in.nextLine();
+                System.out.println("Artista: ");
+                tempArtista = in.nextLine();
+                System.out.println("Numero de musicas: ");
+                tempNumDeMusicas = in.nextInt();
+                System.out.println("Selo: ");
+                tempSelo = in.nextLine();
+                p[i] = new CD(tempArtista,tempNumDeMusicas,tempSelo,tempoNum,tempEstoque,tempNome,tempPreco,tempAtivo);
+    }
+    
+    public static void addDVDToInventory(Produto[]p, Scanner in, int i){
+        int tempoNum;
+        int tempEstoque;
+        String tempNome;
+        double tempPreco;
+        String tempAtivo;
+        int tempDuracaoFilme;
+        int tempClassificacaoEtaria;
+        String tempEstudioCinematografico;
+                System.out.println("Insira o numero do item: ");
+                tempoNum = in.nextInt();
+                System.out.println("Insira a quantidade em estoque: ");
+                tempEstoque = in.nextInt();
+                in.nextLine();
+                System.out.println("Insira o nome do produto: ");
+                tempNome = in.nextLine();
+                System.out.println("Valor do produto: ");
+                tempPreco = in.nextDouble();
+                in.nextLine();
+                System.out.println("Status do produto: ");
+                tempAtivo = in.nextLine();
+                System.out.println("Duracao: ");
+                tempDuracaoFilme = in.nextInt();
+                System.out.println("Classificacao indicativa: ");
+                tempClassificacaoEtaria = in.nextInt();
+                System.out.println("Estudio produtor: ");
+                tempEstudioCinematografico = in.nextLine();
+                p[i] = new DVD(tempDuracaoFilme,tempClassificacaoEtaria,tempEstudioCinematografico,tempoNum,tempEstoque,tempNome,tempPreco,tempAtivo);
     }
     
     public static int getNumProducts(Scanner in){
@@ -102,7 +164,7 @@ public class Inventário {
     
     public static int getProductNumber(Produto[]p, Scanner in){
         int productChoice = -1;
-        for(int i = 0; i < p.length-1; i++){
+        for(int i = 0; i < p.length ; i++){
             System.out.println("Opcao " + i + " - " + p[i].getNomeProd());
         }
         while(productChoice < 0 || productChoice >= p.length){
@@ -111,32 +173,42 @@ public class Inventário {
             }
             catch(InputMismatchException e){
                 System.out.println("Digite um indice valido ");
+                System.out.println("Escolha um produto");
                 in.nextLine();
             }
         }
         return productChoice;
     }
     
-    public static void addInventory(Produto[]p, Scanner in){
-        int productChoice;
-        int updateValue = -1;
-        productChoice = getProductNumber(p,in);
-        while(updateValue < 0){
+    public static void addInventory(Produto[]p, Scanner in, int i){
+        int stockChoice = -1;
+        while(stockChoice < 1 || stockChoice > 2){
             try{
-                System.out.println("Qauntos produtos deseja adicionar? ");
-                updateValue = in.nextInt();
-                if(updateValue < 0){
-                    System.out.println("Digite um valor maior ou igual a zero.");
+                System.out.println("1: CD");
+                System.out.println("2: DVD");
+                System.out.print("Insira o tipo de produto: ");
+                stockChoice = in.nextInt();
+
+                if(stockChoice < 1 || stockChoice > 2){
+                    System.out.println("Somente os números 1 ou 2 são permitidos!");
                 }
-                
             }
-            catch(InputMismatchException e){
-                System.out.println("Digite uma quantia valida");
-                in.nextLine();
+        catch(InputMismatchException e){
+
+            System.out.println("Digite apenas números.");
+
+            in.nextLine();
+
+            stockChoice = -1;
             }
         }
-        p[productChoice].addToInventory(updateValue);
-        
+        if(stockChoice == 1){
+
+            addCDToInventory(p,in,i);
+        }
+        else{
+            addDVDToInventory(p,in,i);
+        }
     }
     
     public static void deductInventory(Produto[]p, Scanner in){
@@ -167,7 +239,7 @@ public class Inventário {
         
     }
     
-    public static void executeMenuChoice( int getMenuOption,Produto[]p, Scanner in){
+    public static void executeMenuChoice( int getMenuOption,Produto[]p, Scanner in, int i){
         switch(getMenuOption){
                 case 1:
                     System.out.println("\n == Exibir inventario == ");
@@ -175,7 +247,7 @@ public class Inventário {
                     break;
                 case 2:
                     System.out.println("\n== Adicionar Estoque ==");
-                    addInventory(p,in);
+                    addInventory(p,in,i);
                     break;
                 case 3:
                     System.out.println("\n == Deduzir Estoque == ");
